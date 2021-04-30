4fc7b154b553bf960b52420cc13f4adc83994e9d
kr1stj0n <kr1stj0n@pm.me>
2021-04-30 17:47:05 +0200

    fixed matplotlib import

--- plot.py
+++ plot.py
@@ -39,6 +39,8 @@ from fabric.api import task, warn, put, puts, get, local, run, execute, \
 
 import numpy as np
 import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
