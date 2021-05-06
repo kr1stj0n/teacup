d56f613de140d1cfd9fb9c3e109d6a2a975ccfc2
kr1stj0n <kr1stj0n@pm.me>
2021-05-06 18:47:53 +0200

    small fix

--- loggers.py
+++ loggers.py
@@ -639,7 +639,7 @@ def start_qdisc_logger(file_prefix='', remote_dir='', local_dir='.'):
         pass
 
     # run qdisc_logger.sh script
-    pid = runbg('qdisc_logger.sh %f %s %s %s' % (sample_interval, 'ifb1', 'pie',
+    pid = runbg('qdisc_logger.sh %f %s %s %s' % (sample_interval, 'ifb1', 'shq',
                                               logfile))
 
     bgproc.register_proc_later(
